b09973378a5a5b11fe3cd0e5552df09d3573f3ae
Maniccia <mariamaniccia@gmail.com>
2018-11-05 13:09:04 -0500

    some html/js

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+//  My variables
+
+var wordBank = ["red", "blue", "yellow"]
+var wins = 0;
+var loss = 0;
+var wrongLetter = [];
+var guessesLeft = 9;
+var underScores = [];
+var userGuesses = [];
+var choosenWord;
+
+// My functions
+
+function startGame(){
+    // picks random word
+   choosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(choosenWord);
+
+//pushes a '_' for every letter of the choosenWord
+    for(var i=0; i < choosenWord.length; i++){
+    underScores.push('_');
+     }
+//prints those "_"s
+console.log(underScores);
+Document.getElementById('spaceHolders').textContent = underScores;
+}
+
+
+// Main 
+
+startGame();
