c3ce982772519ae705a0f569a7b2fa5a0bc5a253
Maniccia <mariamaniccia@gmail.com>
2018-11-08 13:52:40 -0500

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 
 // My Var
-var kingdoms = ["Cap", "Cascade", "Sand", "Lake", "Wooded", "Cloud", "Lost", "Metro", "Snow", "Seaside", "Luncheon", "Ruined", "Bowsers","Moon","DarkSide", "DarkerSide", "Mushroom"];
+var kingdoms = ["cap", "cascade", "sand", "lake", "wooded", "cloud", "lost", "metro", "snow", "seaside", "luncheon", "ruined", "bowsers","moon","darkside", "darkerside", "Mushroom"];
 var wins = 0;
 var loss = 0;
 var wrongLetter = [];
-var guessesLeft = 9;
+var guessesLeft = [];
 var underScores = [];
 var userGuesses = [];
 var choosenWord;
+var winCount = 0;
 
 // My functions
 
@@ -20,31 +21,66 @@ function startGame(){
     for(var i=0; i < choosenWord.length; i++){
     underScores.push('_');
      }
-// doesn't work ...prints those "_"s !!!
-console.log(underScores);
-document.getElementById('spaceHolders').textContent = underScores;
+//prints those "_"s !!!
+document.getElementById('spaceHolders').textContent = underScores.join(" ");
 
 // start new
 wrongLetter = [];
 guessesLeft = 10;
 
-// doesnt work AGAIN!!! print on browser
+// print on browser
 console.log(guessesLeft);
-document.getElementById(guessLeft).textContent = guessesLeft;
-   
+document.getElementById('guessLeft').textContent = guessesLeft;
+  
 }
 
-// keyboard
+function winLose(){
+
+if(winCount === choosenWord.length){
+alert("Winner!");
+}
+else if (guessesLeft === 0){
+    alert("Loser!");
+}
+}
+
+
+// User Guesses
 document.onkeyup = function(event){
-// registers the key pressed
+
+    // registers the key pressed
     userGuesses = event.key;
     console.log(userGuesses);
 
-    if(userGuesses === choosenWord){
-        console.log('yes');
-    }
-}
+    // does it exsist in the choosen word
+    if(choosenWord.indexOf(userGuesses) > -1){
 
+        for(var i = 0; i < choosenWord.length; i++){
+
+            if(choosenWord[i] === userGuesses){
+
+                underScores[i] = userGuesses;
+                console.log(underScores);
+                document.getElementById('spaceHolders').textContent = underScores.join(" ");
+                winCount++;
+                winLose();
+            }
+        }
+
+    }
+
+    else{
+wrongLetter.push(userGuesses);
+guessesLeft--;
+console.log(guessesLeft);
+document.getElementById('guessLeft').textContent = guessesLeft;
+console.log(wrongLetter);
+document.getElementById('wrongGuess').textContent = wrongLetter;
+winLose();
+
+    }
+
+}
 
 
 
